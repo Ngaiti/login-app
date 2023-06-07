84254eee29b15763776d08fa7ff93bce77632aab
Ngaiti <henfanar@gmail.com>
2023-06-07 10:38:50 +0800

    Fixed Login page typo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
                         onChange={(e) => setUsername(e.target.value)}
                     />
                     <Form.Text className="text—muted">
-                        Well never share your email with anyone else.
+                        We&apos;ll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
