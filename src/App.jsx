ec88f238ccf162b96f421cd90b688212bef6661a
Ngaiti <henfanar@gmail.com>
2023-06-07 12:02:34 +0800

    Authentican token/Local storage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import { useState } from "react";
 import { AuthContext } from "./AuthContext";
+import RequireAuth from "./components/RequireAuth";
 
 export default function App() {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useLocalStorage("token", null);
   return (
     <AuthContext.Provider value={{ token, setToken }}>
       <BrowserRouter>
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Login />} path="/login" />
-          <Route element={<Dashboard />} path="/dashboard" />
+          <Route
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+            path="/dashboard" />
         </Routes>
       </BrowserRouter>
     </AuthContext.Provider>
