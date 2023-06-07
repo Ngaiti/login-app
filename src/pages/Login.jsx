3691eb22585eaf7c7729c1bd09b2b25a56628310
Ngaiti <henfanar@gmail.com>
2023-06-07 10:35:14 +0800

    Login page added

---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Button, Container, Form, FormGroup } from "react-bootstrap";
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <Container>
+            <h1 className="my-3">Login to your account</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Form.Text className="text—muted">
+                        Well never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <FormGroup className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </FormGroup>
+                <Button variant="primary">Login</Button>
+            </Form>
+        </Container>
+    )
+}
